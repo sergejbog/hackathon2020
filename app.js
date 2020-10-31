329342c667644490096ba10812d19f792120575e
Arsovskii <arsoskidavid4@gmail.com>
2020-10-31 16:39:45 +0100

    homepage

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 
 const month = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
 
-app.use(express.urlencoded());
+app.use(express.urlencoded({extended : false}));
 app.use(express.json());
 
 app.use(express.static('css'));
